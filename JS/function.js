22c28a87cd91b6beae9de6d678550ee254a0df9f
Gonzalo Puigcernau <gonzalopuigcernau@gmail.com>
2023-02-01 12:54:55 -0300

    Primer Commit

---
+++ JS/function.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick=function(){
+    console.log("depuracio: demuestra que tomó el click");
+    document.getElementById("demo").innerHTML="Tomá pavó, anduvo";
+}
